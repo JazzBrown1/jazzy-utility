8cd0713a0d60a7710bbd3c36a953d88ce3d7e759
Your Name <jazz@tezle.com>
2019-09-05 20:10:23 +0100

    Array Delete bug fix

---
+++ src/jazzyArray.js
@@ -0,0 +1,11 @@
+const arrayDelete = (arr, item) => {
+  if (!Array.isArray(arr)) return false;
+  const _index = arr.findIndex((_item) => item === _item);
+  if (_index !== -1) {
+    arr.splice(_index, 1);
+    return _index;
+  }
+  return -1;
+};
+
+export default arrayDelete;
